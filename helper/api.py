98729ef1051593a328776aa8dfa898f507d40351
Маргарита Любимова <margarita_lybim@list.ru>
2020-03-20 15:24:15 +0100

    add tests

--- helper/api.py
+++ helper/api.py
@@ -8,32 +8,33 @@ class JsonPlaceholderAPI:
     def get_posts(self):
         url = self.base_url + '/posts'
         response = requests.get(url)
-        return response.json()
+
+        return response
 
     def get_post_with_post_id(self, post_id):
         url = self.base_url + '/posts/{}'.format(post_id)
         response = requests.get(url)
-        return response.json()
+        return response
 
     def get_comments_with_post_id(self, post_id):
-        url = self.base_url + '/posts/{}'.format(post_id)
+        url = self.base_url + '/comments?postId={}'.format(post_id)
         response = requests.get(url)
-        return response.json()
+        return response
 
     def get_post_with_user_id(self, user_id):
         url = self.base_url + '/posts?userId={}'.format(user_id)
         response = requests.get(url)
-        return response.json()
+        return response
 
     def create_post(self, param):
         url = self.base_url + '/posts'
-        response = requests.post(url, param)
-        return response.json()
+        response = requests.post(url, data=param)
+        return response
 
-    def delete_post(self, user_id):
-        url = self.base_url + '/posts/{}'.format(user_id)
+    def delete_post(self, post_id, user_id):
+        url = self.base_url + '/posts/{}?userId={}'.format(post_id, user_id)
         response = requests.delete(url)
-        return response.json()
+        return response
 
 
 
